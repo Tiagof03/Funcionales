0f4238fb55c7175d23f11634b9d24d563a706bee
Tiago <tiagohfunes03@gmail.com>
2025-09-30 17:39:38 -0300

    Add files via upload

---
+++ Programacion_funcional/src/Actividad_2/Producto.java
@@ -0,0 +1,47 @@
+package Actividad_2;
+
+public class Producto {
+    private String nombre;
+    private String categoria;
+    private Double precio;
+    private int stock;
+
+    public Producto(String nombre, String categoria, Double precio, int stock) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
